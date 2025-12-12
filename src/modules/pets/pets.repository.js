cedd76d87c065b12bfa68a774736bba6769260f2
Matheus Falcão <matheusfalcao695@gmail.com>
2025-12-12 01:51:22 -0300

    adicionando query customizada para a lista de pets

--- src/modules/pets/pets.repository.js
+++ src/modules/pets/pets.repository.js
@@ -7,6 +7,23 @@ export default class PetsRepository extends BaseRepository {
         super(db, "pets");
     }
 
+    async findAll() {
+        const result = await this.execute(
+            `SELECT pet.id
+              , pet.name
+              , pet.breed
+              , pet.species
+              , pet.birthdate
+              , owner.id AS owner_id
+              , owner.name AS owner_name
+           FROM ${this.table} AS pet
+           INNER JOIN owners owner ON pet.owner_id = owner.id 
+           ORDER BY pet.name ASC`
+        )
+
+        return result;
+    }
+
     async existsByOwnerId(owner_id) {
         const result = await this.execute(`SELECT * FROM ${this.table} WHERE owner_id = ?`, [owner_id]);
         return !!result.length;
